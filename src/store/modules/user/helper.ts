c2b66363177d8261c0cd26ca3d97cf5196d971a6
Wason-Fok <32887251+Wason-Fok@users.noreply.github.com>
2023-04-26 10:42:44 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -16,8 +16,8 @@ export function defaultSetting(): UserState {
   return {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      name: 'Wason',
+      description: 'Follow <a href="https://github.com/wason-fok" class="text-blue-500" target="_blank" >GitHub</a>',
     },
   }
 }
